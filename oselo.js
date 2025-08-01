4ed701cdb37a0bbb0c620b27cfa1b77b5b048fcc
Takahiro-Hane <futtosal.1219@icloud.com>
2025-08-01 10:13:38 +0900

    Add files via upload

---
+++ oselo.js
@@ -0,0 +1,28 @@
+'use script'
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let squares = document.querySelectorAll(".square");
+
+//     for (let i = 0; i < squares.length; i++) {
+//         squares[i].addEventListener("click", function() {
+//             for (let j = 0; j < squares.length; j++) {
+//                 squares[j].classList.remove("selected");
+//             }
+//             this.classList.add("selected");
+//         });
+//     }
+// });
+
+// $(function () {
+//     $(".square").click(SquareEvent);
+// });
+
+// function SquareEvent() {
+//     let square = $(this);
+
+//     putPiece(square, "black");
+// }
+
+// function putPiece(targetSquare, player) {
+//     targetSquare.text("●") .attr("data3". player).addClass("selected");
+// }
